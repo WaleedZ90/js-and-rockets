8ef716cbd756e9a71173f53ce949eef8c063ac16
Waleed Tayseer <waleedtayseer@hotmail.com>
2021-10-01 14:34:24 +0200

    Applied filtering logic

--- src/solution.js
+++ src/solution.js
@@ -1,3 +1,5 @@
+const { orderBy } = require("lodash");
+
 // Please implement your solution in this file
 const fetchFlights = () => {
 	return fetch("https://api.spacexdata.com/v3/launches/past")
@@ -5,11 +7,46 @@ const fetchFlights = () => {
 		.then(prepareData);
 };
 
+const includesNasa = (flight) => {
+	if (
+		!flight ||
+		!flight.rocket ||
+		!flight.rocket.second_stage ||
+		!flight.rocket.second_stage.payloads ||
+		flight.rocket.second_stage.payloads.length === 0
+	) {
+		return false;
+	}
+
+	const payloads = flight.rocket.second_stage.payloads;
+
+	for (const payload of payloads) {
+		if (
+			payload.customers.includes("NASA") ||
+			payload.customers.includes("NASA (CRS)")
+		) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
 const prepareData = (flights) => {
-	return flights.map((flight) => {
-		const { flight_number, mission_name, payloads_count } = flight;
-		return { flight_number, mission_name, payloads_count };
+	const parsedArray = [];
+	flights.forEach((flight) => {
+		if (flight.launch_year == "2018" && includesNasa(flight)) {
+			const { flight_number, mission_name } = flight;
+			const payloads_count = flight.rocket.second_stage.payloads.length;
+			parsedArray.push({ flight_number, mission_name, payloads_count });
+		}
 	});
+
+	return orderBy(
+		parsedArray,
+		["payloads_count", "flight_number"],
+		["desc", "desc"]
+	);
 };
 
 const renderData = (flights) => {
